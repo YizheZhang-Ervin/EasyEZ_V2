68562ecadcb273cd188fbfdd80d7217c6526c57a
Yizhe Zhang <zhang.yizhe@husky.neu.edu>
2020-03-20 23:47:16 -0400

    fix bug

--- EasyEZ/settings.py
+++ EasyEZ/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dr6-w(_7ttw$o$0h_ewpv975-jduet@(8-3!urfkymoa*j8wpq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
